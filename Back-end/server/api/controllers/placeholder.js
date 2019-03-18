c08da89f50e2e9cb6588312a3adb9fc679e144b3
yungvinsantos <vinnysantos559@gmail.com>
2019-03-17 17:13:07 -0700

    adding placeholders

---
+++ Back-end/server/api/controllers/placeholder.js
@@ -0,0 +1,6 @@
+/*README: This file is a placeholder to maintain project structure.
+          Github does not keep empty directories. 
+          Delete file once current directory is populated. 
+*/
+
+module.exports = placeholder;
