7568e5c5d658d005b9b83ab49dfe20b15fd102fa
Cory <cjl9703@yahoo.com>
2019-04-09 23:28:00 -0700

    Database connected to app.js and global DATABASE variable created

--- Back-end/server/app.js
+++ Back-end/server/app.js
@@ -1,10 +1,52 @@
 const express = require('express');
-const app = express();    
+var mysql = require('mysql');
+const app = express();
 
 //morgan is used to console log requests from client
 const morgan = require('morgan'); 
 //body-parser is used to parse json bodies from client requests
 const bodyParser = require('body-parser'); 
+
+//Connect database
+
+const mysql_config = {
+    host: '54.183.219.162',
+    user: 'csc667',
+    password: 'password',
+    port: 3306,
+    database: 'eventupdb',
+  };
+
+  class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
+
+global.DATABASE = new Database(mysql_config);
+
+DATABASE.connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Database Connected! ");
+  });
 
 const apiRoutes = require('./src/routes/apiRoutes');
 
