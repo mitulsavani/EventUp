2ad5fe3a5805c5a9e65e6643fe88fb399e39bbc6
yungvinsantos <vinnysantos559@gmail.com>
2019-04-13 16:30:22 -0700

    added password hashing and authentication

--- Back-end/server/api/controllers/users.js
+++ Back-end/server/api/controllers/users.js
@@ -1,38 +1,81 @@
 const db = require('../models/database.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.register = (req, res, next) => {
-    db.query('INSERT INTO User SET ?', 
-    {FirstName: req.body.FirstName, LastName: req.body.LastName, Email: req.body.Email, Password: req.body.Password})
-    .then(() => {
-        res.send({
-            message: 'success'
+    bcrypt.hash(req.body.Password, 10, (err, hash) => {
+        if(err) {
+            return res.status(500).json({
+                error: err
+            });
+        }
+        else {
+            db.query('INSERT INTO User SET ?', 
+            {FirstName: req.body.FirstName, LastName: req.body.LastName, Email: req.body.Email, Password: hash})
+            .then(() => {
+                res.send({
+                    message: 'success'
+                });
+            })
+            .catch(err => {
+                res.send({
+                    message: 'error',
+                    error: err
+                });
+            })
+            }
         });
-    })
-    .catch(err => {
-        res.send({
-            message: 'error',
-            error: err
-        });
-    })
 }
 
 exports.login = (req, res, next) => {
-    db.query('SELECT * FROM User WHERE Email = ? AND Password = ?', [req.body.Email,req.body.Password])
-    .then(data => {
+    db.query('SELECT * FROM User WHERE Email = ?', req.body.Email)
+    .then(user => {
         //No User Found Check
-        if(data == "") {
-            res.send({
-                message: 'error'
+        if(user == "") {
+            res.status(404).json({
+                message: 'User not found'
             });
         }
-        res.send({
-            data: data,
-            message: 'success'
-        })
+        bcrypt.compare(req.body.Password, user[0].Password, (err, result) => {
+            if(result) {
+                const token = jwt.sign(
+                    {
+                        id: user.id,
+                        FirstName: user.FirstName,
+                        LastName: user.LastName,
+                        Email: user.Email,
+                    },
+                    process.env.JWT_KEY,
+                    {
+                        expiresIn: '24h'
+                    }
+                )
+                res.status(200).json({
+                    message: 'Successful login',
+                    token: token
+                });
+            }
+            else {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+        });
     })
     .catch(err => {
         res.send({
-            message:'error'
+            message: err
         })
     })
+}
+
+exports.getUsers = (req, res, next) => {
+    db.query('SELECT * FROM User')
+    .then(users => {
+        const response = {
+            UserCount: users.length,
+            users: users
+        }
+        res.status(200).json(response);
+    });
 }
--- Back-end/server/app.js
+++ Back-end/server/app.js
@@ -13,7 +13,16 @@ db.connection.connect(function(err) {
     console.log("Database Connected Successfully!");
   });
 
-
+app.use((req, res, next) => { 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if(req.method == 'OPTIONS'){ 
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+})
+  
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
