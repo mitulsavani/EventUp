56fbcef355b2aa11ab41892febe2775a04d7d925
mitulsavani <mitul.savani2@yahoo.com>
2019-05-10 01:35:10 -0700

    RSVP button works, making a post request to backend

--- Front-end/src/screens/DetailEventScreen.js
+++ Front-end/src/screens/DetailEventScreen.js
@@ -6,14 +6,16 @@ import {
   ScrollView,
   Image,
   TouchableOpacity,
-  Alert
+  Alert,
+  AsyncStorage,
+  SafeAreaView
 } from "react-native";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import { Button } from "react-native-elements";
 import moment from "moment";
 import { format } from "date-fns";
-import MapView, { Marker } from 'react-native-maps';
-import openMap, { createOpenLink } from 'react-native-open-maps';
+import MapView, { Marker } from "react-native-maps";
+import openMap, { createOpenLink } from "react-native-open-maps";
 import { Permissions, Calendar, Localization, Alarm } from "expo";
 
 export default class DetailEventScreen extends React.Component {
@@ -59,9 +61,63 @@ export default class DetailEventScreen extends React.Component {
     }
   }
 
-  onTicketButtonPress = () => {
-    console.log("button pressed");
-  };
+  async onTicketButtonPress() {
+    const { event } = this.state;
+    try {
+      const token = await AsyncStorage.getItem("userToken");
+
+      try {
+        let response = await fetch(
+          "http://ec2-54-183-219-162.us-west-1.compute.amazonaws.com:3000/users/RSVP",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json; charset=utf-8",
+              Authorization: token
+            },
+
+            body: JSON.stringify({
+              UserId: event.UserId,
+              EventId: event.id
+            })
+          }
+        );
+
+        response.json().then(result => {
+          console.log(result);
+
+          if (result.status == true) {
+            Alert.alert(
+              "Alert!",
+              "Successfully sent your RSVP sent to host",
+              [
+                {
+                  text: "OK",
+                  onPress: () => this.props.navigation.navigate("events")
+                }
+              ],
+              { cancelable: false }
+            );
+          } else {
+            Alert.alert("Alert!", "Failed to RSVP, Please try again later", [{ text: "OK" }], {
+              cancelable: false
+            });
+          }
+        });
+      } catch (e) {
+        console.log("Something failed with response", e);
+
+        Alert.alert(
+          "Alert!",
+          "Error, Server Issue",
+          [{ text: "OK" }],
+          { cancelable: false }
+        );
+      }
+    } catch (e) {
+      console.log("AsynStorage failed", e);
+    }
+  }
 
   loadingView = () => {
     return (
@@ -114,106 +170,112 @@ export default class DetailEventScreen extends React.Component {
 
     return (
       <View style={styles.mainContainer}>
-        <ScrollView style={styles.scrollViewContainer}>
-          <View style={styles.bannerImageContainer}>
-            <Image
-              source={require("../img/sample_image.jpg")}
-              style={{
-                flex: 1,
-                height: 200,
-                width: "100%"
-              }}
-              resizeMode="cover"
-            />
-          </View>
-          {/* bannerImageContainer End */}
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+          <ScrollView style={styles.scrollViewContainer}>
+            <View style={styles.bannerImageContainer}>
+              <Image
+                source={require("../img/sample_image.jpg")}
+                style={{
+                  flex: 1,
+                  height: 200,
+                  width: "100%"
+                }}
+                resizeMode="cover"
+              />
+            </View>
+            {/* bannerImageContainer End */}
 
-          <View style={styles.generalInformationContainer}>
-            <Text style={styles.generalInformationHeaderTitleStyle}>
-              {event.Name}
-            </Text>
-            <Text style={styles.byTextStyle}>{event.CategoryName}</Text>
+            <View style={styles.generalInformationContainer}>
+              <Text style={styles.generalInformationHeaderTitleStyle}>
+                {event.Name}
+              </Text>
+              <Text style={styles.byTextStyle}>{event.CategoryName}</Text>
 
-            <TouchableOpacity
-              style={styles.detailContainer}
-              onPress={() => this.onAddCalendarEvent(event)}
-              activeOpacity={0.8}
-            >
-              <SimpleLineIcons name="calendar" size={25} />
-              <View style={styles.subDetailColumnContainer}>
-                <Text style={styles.detailMainText}>
-                  {moment.utc(event.StartDate).format("MMMM DD")}
-                </Text>
-                <Text style={styles.detailSubText}>
-                  {format("January 01, 2019 " + event.StartTime, "hh:mm a")}
-                </Text>
+              <TouchableOpacity
+                style={styles.detailContainer}
+                onPress={() => this.onAddCalendarEvent(event)}
+                activeOpacity={0.8}
+              >
+                <SimpleLineIcons name="calendar" size={25} />
+                <View style={styles.subDetailColumnContainer}>
+                  <Text style={styles.detailMainText}>
+                    {moment.utc(event.StartDate).format("MMMM DD")}
+                  </Text>
+                  <Text style={styles.detailSubText}>
+                    {format("January 01, 2019 " + event.StartTime, "hh:mm a")}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+
+              <View style={styles.detailContainer}>
+                <SimpleLineIcons name="location-pin" size={25} />
+                <View style={styles.subDetailColumnContainer}>
+                  <Text style={styles.detailMainText}>
+                    {event.LocationName}
+                  </Text>
+                  {/* <Text style={styles.detailSubText}>{event.venue.address}</Text> */}
+                </View>
               </View>
-            </TouchableOpacity>
 
-            <View style={styles.detailContainer}>
-              <SimpleLineIcons name="location-pin" size={25} />
-              <View style={styles.subDetailColumnContainer}>
-                <Text style={styles.detailMainText}>{event.LocationName}</Text>
-                {/* <Text style={styles.detailSubText}>{event.venue.address}</Text> */}
+              <View style={styles.detailContainer}>
+                <SimpleLineIcons name="tag" size={25} />
+                <View style={styles.subDetailColumnContainer}>
+                  <Text style={styles.detailMainText}>Free</Text>
+                  <Text style={styles.detailSubText}>on EventUp</Text>
+                </View>
               </View>
             </View>
+            {/* generalInformationContainer End */}
 
-            <View style={styles.detailContainer}>
-              <SimpleLineIcons name="tag" size={25} />
-              <View style={styles.subDetailColumnContainer}>
-                <Text style={styles.detailMainText}>Free</Text>
-                <Text style={styles.detailSubText}>on EventUp</Text>
+            <View style={styles.aboutEventContainer}>
+              <Text style={styles.aboutTitleStyle}>About</Text>
+              <Text
+                style={styles.descriptionStyle}
+                numberOfLines={4}
+                ellipsizeMode="tail"
+              >
+                {event.Description}
+              </Text>
+            </View>
+            {/* aboutEventContainer End */}
+
+            <View style={styles.locationContainer}>
+              <Text style={styles.locationTitleStyle}>Location</Text>
+              <Text style={styles.locationSubTitleStyle}>
+                {event.LocationName}
+              </Text>
+              <View style={styles.mapImageContainer}>
+                <MapView
+                  style={{ flex: 1 }}
+                  region={{
+                    latitude: 42.882004,
+                    longitude: 74.582748,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421
+                  }}
+                  showsUserLocation={true}
+                >
+                  <MapView.Marker
+                    coordinate={{ latitude: 42.882004, longitude: 74.582748 }}
+                    onPress={createOpenLink({
+                      start: "New York City, New York, NY",
+                      travelType: "drive",
+                      end: "SOHO, New York, NY"
+                    })}
+                  />
+                </MapView>
               </View>
             </View>
-          </View>
-          {/* generalInformationContainer End */}
-
-          <View style={styles.aboutEventContainer}>
-            <Text style={styles.aboutTitleStyle}>About</Text>
-            <Text
-              style={styles.descriptionStyle}
-              numberOfLines={4}
-              ellipsizeMode="tail"
-            >
-              {event.Description}
-            </Text>
-          </View>
-          {/* aboutEventContainer End */}
-
-          <View style={styles.locationContainer}>
-            <Text style={styles.locationTitleStyle}>Location</Text>
-            <Text style={styles.locationSubTitleStyle}>
-              {event.LocationName}
-            </Text>
-            <View style={styles.mapImageContainer}>
-            <MapView
-              style={{flex: 1}}
-              region={{
-                latitude: 42.882004,
-                longitude: 74.582748,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-              }}
-              showsUserLocation={true}
-            >
-            <MapView.Marker
-                coordinate= {
-                  {latitude: 42.882004,
-                  longitude: 74.582748}
-                }
-                onPress= { createOpenLink({start: "New York City, New York, NY", travelType: "drive", end: "SOHO, New York, NY"}) }
-            />
-            </MapView>
-            </View>
-          </View>
-          {/* locationContainer End */}
-        </ScrollView>
+            {/* locationContainer End */}
+          </ScrollView>
+        </SafeAreaView>
         <View style={styles.purchaseContainer}>
           <TouchableOpacity>
             <Button
               onPress={() => this.onTicketButtonPress()}
               title="RSVP"
               buttonStyle={styles.rsvpButton}
+              titleStyle={{ fontSize: 25, fontFamily: "Futura" }}
               rounded
             />
           </TouchableOpacity>
@@ -226,7 +288,7 @@ export default class DetailEventScreen extends React.Component {
     const { isLoading } = this.state;
     return (
       <View style={styles.mainContainer}>
-        {isLoading ? this.loadingView() : this.contentView() }
+        {isLoading ? this.loadingView() : this.contentView()}
       </View>
     );
   }
@@ -331,13 +393,14 @@ const styles = StyleSheet.create({
 
   purchaseContainer: {
     width: "100%",
-    height: 50,
-    alignItems: "center"
+    height: 60,
+    alignItems: "center",
+    marginBottom: 5
   },
 
   rsvpButton: {
-    width: 250,
-    borderRadius: 5,
-    backgroundColor: "#E8787B"
+    width: 375,
+    height: 70,
+    backgroundColor: "#39CA74"
   }
 });
